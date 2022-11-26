a7a2ac1ddaa4f92ff2d45f3b94a4910abf5bf42d
Ike Maldonado <maldonadoike@gmail.com>
2022-11-26 15:47:42 -0800

    added bounce to click

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,10 +1,17 @@
 import { Canvas, useFrame } from "@react-three/fiber";
 import { useRef, useState } from "react";
 import { Mesh } from "three";
+import { useSpring, animated, config } from "@react-spring/three"
 
 function Box() {
   const boxRef = useRef<Mesh>(null!);
   const [active, setActive] = useState(false);
+
+  // Provides bounce when clicked
+  const { scale } = useSpring({
+    scale: active ? 1.5 : 1,
+    config: config.wobbly
+  });
 
   useFrame(() => {
     boxRef.current.rotation.x += 0.005;
@@ -13,13 +20,14 @@ function Box() {
   });
 
   return (
-    <mesh
-    scale={active ? 1.5 : 1}
-    onClick={() => setActive(!active)}
-    ref={boxRef}>
+    <animated.mesh
+      scale={scale}
+      onClick={() => setActive(!active)}
+      ref={boxRef}
+    >
       <boxGeometry args={[1, 1, 1]} />
       <meshPhongMaterial color={"royalblue"} />
-    </mesh>
+    </animated.mesh>
   );
 }
 
